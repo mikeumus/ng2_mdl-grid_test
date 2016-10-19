b7a95727dc6f24a1e39fe149de6287327c32ebb3
darkwing-duck <darkwing-duck@darkwing-ducks-MacBook-Pro.local>
2016-10-19 17:41:46 -0400

    Init `mdl-grid` with Material2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-// import { MdToolbar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  title = 'Bloomberg Dataflow Dashboard';
+  title = 'Integration Console';
 }
